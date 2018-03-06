79ba74bd9127ab580cfb3c5ca62b616c17b3e869
Arkanius <vtr.gomes@convenia.com.br>
2018-03-05 23:56:33 -0300

    starting cleaning data

---
+++ pro_proccess.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+data = pd.read_csv('credit-data.csv')
+data.describe()
+
+data.loc[data['age'] < 0]
+
+data.drop(data[data.age < 0].index, inplace=True)
+
+data['age'][data.age > 0].mean()
+data.loc[data['age'] < 0]
+
+pd.isnull(data['age'])
+
+data.loc[pd.isnull(data['age'])]
+
+prev = data.iloc[:, 1:4].values
+nclass = data.iloc[:, 4].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(prev[:, 0:3])
+prev[:, 0:3] = imputer.transform(prev[:, 0:3])
+
+print(prev)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(prev)
